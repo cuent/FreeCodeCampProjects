a3b11f41338c451bdbddb3a0f94babdbcc3c5047
cuent <c.uent@hotmail.com>
2016-05-26 10:45:17 -0500

    Finished Wikipedia viewer project

--- Build a Wikipedia Viewer/myjavascript.js
+++ Build a Wikipedia Viewer/myjavascript.js
@@ -1,17 +1,32 @@
-$.ajax({
-  url: "https://en.wikipedia.org//w/api.php?action=query&format=json&prop=extracts%7Cpageimages&generator=search&exsentences=1&exlimit=20&exintro=1&explaintext=1&pilimit=50&gsrsearch=computer&gsrnamespace=0&gsrlimit=5",
-  dataType: "jsonp",
-  success: function (data) {
-    console.log(data.query.pages)
-    $.each(data.query.pages,function(i,e){
-        console.log("URL: "+"https://en.wikipedia.org/?curid="+e.pageid)
-        console.log("Title: "+e.title);
-        console.log("Extract: "+e.extract);
+function getArticles(keyword) {
+    $.ajax({
+        url: "https://en.wikipedia.org//w/api.php?action=query&format=json&prop=extracts%7Cpageimages&generator=search&exsentences=1&exlimit=20&exintro=1&explaintext=1&pilimit=50&gsrsearch=" + keyword + "&gsrnamespace=0&gsrlimit=5",
+        dataType: "jsonp",
+        success: function (data) {
+            $.each(data.query.pages, function (i, e) {
+                $("#results").append('<li><a target="_blank" href="https://en.wikipedia.org/?curid=' + e.pageid + '"><h2>' + e.title + '</h2><span>' + e.extract + '</span></a></li>');
+            });
+        }
     });
-  }
+
+}
+
+$('#searchText').keypress(function (e) {
+    if (e.keyCode == 13)
+        makeSearch();
 });
 
-
-$(document).ready(function(){
-
+$("#search").button().click(function () {
+    makeSearch();
 });
+
+function makeSearch() {
+    var textToSearch = $("#searchText").val();
+
+    if (textToSearch != "") {
+        $("ul").empty();
+        getArticles(textToSearch);
+    } else {
+        $("ul").empty();
+    }
+}
